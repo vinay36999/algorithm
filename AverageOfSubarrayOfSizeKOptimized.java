427b03f27369b299dd81c11bab8b947de15d61a6
vinay36999 <vinay36999@gmail.com>
2021-02-14 14:44:56 -0800

    Update AverageOfSubarrayOfSizeKOptimized.java

--- AverageOfSubarrayOfSizeKOptimized.java
+++ AverageOfSubarrayOfSizeKOptimized.java
@@ -11,18 +11,18 @@ public class AverageOfSubarrayOfSizeKOptimized {
     private static double[] findAverages(int sliderLength, int[] array) {
         double[] result = new double[array.length - sliderLength + 1];
         double windowSum = 0;
-        double windowStart = 0;
+        double previousWindowSum = 0;
         for(int index=0; index<array.length-sliderLength + 1; index++){// time complexity : O(array.length)
-            if(windowStart==0){
+            if(previousWindowSum==0){
                 for(int pos=index; pos<index+sliderLength; pos++){ // time complexity : O(K)
                     windowSum += array[pos];
                 }
             }
             else{
-                windowSum=windowStart - array[index-1]+array[index+sliderLength-1];// time complexity : O(array.length)
+                windowSum=previousWindowSum - array[index-1]+array[index+sliderLength-1];// time complexity : O(array.length)
             }
             result[index]=windowSum/sliderLength;
-            windowStart = windowSum;
+            previousWindowSum = windowSum;
         }
         return result;
     }
